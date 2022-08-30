3aafdd02847c73b669964f6b48b4dd47167b0a2a
itmma92 <itmma92@gmail.com>
2022-08-31 01:19:10 +0500

    Initial commit

---
+++ exercises/lucky_ticket.py
@@ -0,0 +1,20 @@
+"""
+Задача - "Счастливый билет"
+Напишите функицю is_lucky_ticket, которая принимает на вход число от пользователя и проверяет, счастливый ли билет
+
+*Счастливые билет - это билет, у которого сумма левой части равна правой, например:
+
+123123 - (1+2+3) = (1+2+3) > True
+123122 - (1+2+3) != (1+2+2) > False
+"""
+
+number_lucky_ticket=(input('Введите номер билета-'))
+def is_lucky_ticket(number_lucky_ticket):
+    list_lucky_ticket=(list(map(int,number_lucky_ticket)))
+    if len(list_lucky_ticket)%2==0:
+        if sum(list_lucky_ticket[0:int(len(list_lucky_ticket)/2)])==sum(list_lucky_ticket[int(len(list_lucky_ticket) / 2):]):
+            print('TRUE')
+        else:print("FALSE")
+    else:print('NO')
+
+is_lucky_ticket(number_lucky_ticket)
